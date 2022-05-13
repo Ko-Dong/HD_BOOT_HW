4d56f027397ded75d604a1659a975fb45931a185
Ko-Dong <68644248+Ko-Dong@users.noreply.github.com>
2022-05-13 11:38:15 +0900

    first commit
    godong

---
+++ HD_boot_project/src/main/java/com/boothw/webapp/HdBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.boothw.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdBootProjectApplication.class, args);
+	}
+
+}
